d5d205a00040f2d2eea25ca9c2c4d923f1cd1e61
Rudaya99 <67676443+Rudaya99@users.noreply.github.com>
2020-07-13 08:38:17 +0300

    ...

--- Знайка/Form4.cs
+++ Знайка/Form4.cs
@@ -21,6 +21,7 @@ namespace Знайка
          ".\\Фауна\\Фауна 1.jpg",".\\Фауна\\Фауна 2.jpg",".\\Фауна\\Фауна 3.jpg",
          ".\\Фауна\\Фауна 4.jpg",".\\Фауна\\Фауна 5.jpg",".\\Фауна\\Фауна 6.jpg",".\\Фауна\\Фауна 7.jpg",".\\Фауна\\Фауна 8.jpg"
     };
+        
 
         public Form4()
         {
@@ -36,7 +37,26 @@ namespace Знайка
         {
             
                a= Image.FromFile(filename[ind]);
-              switch (ind)
+        
+             
+
+        }
+
+        private void Form4_Paint(object sender, PaintEventArgs e)
+        {
+
+            
+           // Draw the image.
+           e.Graphics.DrawImage(a, 0.0F, 0.0F,816.0F, 489.0F);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ind++;
+            if (ind > 7) ind = 7;
+            a = Image.FromFile(filename[ind]);
+            Invalidate();
+            switch (answer[ind])
             {
                 case 0:
                     radioButton1.Checked = false;
@@ -96,23 +116,6 @@ namespace Знайка
 
 
             }
-
-        }
-
-        private void Form4_Paint(object sender, PaintEventArgs e)
-        {
-
-            
-           // Draw the image.
-           e.Graphics.DrawImage(a, 0.0F, 0.0F,816.0F, 489.0F);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            ind++;
-            if (ind > 7) ind = 7;
-            a = Image.FromFile(filename[ind]);
-            Invalidate();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -121,6 +124,66 @@ namespace Знайка
             if (ind < 0) ind = 0;
             a = Image.FromFile(filename[ind]);
             Invalidate();
+            switch (answer[ind])
+            {
+                case 0:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    break;
+                case 1:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = true;
+                    radioButton4.Checked = false;
+                    break;
+                case 2:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = true;
+                    break;
+                case 3:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = true;
+                    break;
+                case 4:
+                    radioButton1.Checked = true;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    break;
+                case 5:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    break;
+                case 6:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    break;
+                case 7:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = true;
+                    radioButton4.Checked = false;
+                    break;
+                case 8:
+                    radioButton1.Checked = true;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    break;
+
+
+
+            }
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
@@ -148,6 +211,11 @@ namespace Знайка
             Form ifrm = new Form3();
             ifrm.Show(); // отображаем Form4
             this.Hide(); // скрываем Form4 (this - текущая форма)
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
